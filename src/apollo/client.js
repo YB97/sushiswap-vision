b4ebb11231676f35ef188464598b184c2d40e9ac
Baskakova Yana <bask-yana@yandex.ru>
2020-10-14 17:34:19 +0300

    change subgraph address

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/chickens-1/chickens'
+    uri: 'https://api.thegraph.com/subgraphs/name/zippoxer/sushiswap-subgraph-fork'
   }),
   cache: new InMemoryCache(),
   shouldBatch: true
